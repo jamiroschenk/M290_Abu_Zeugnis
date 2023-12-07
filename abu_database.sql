d6bf0d076bbe30d7cce685f884b07e5953723c60
jamiro.schenk <schenkj@bzz.ch>
2023-12-07 21:09:01 +0100

    code

--- abu_database.sql
+++ abu_database.sql
@@ -1,1 +1,1 @@
-
+create database jamiro;
--- abu_zeugnisse.js
+++ abu_zeugnisse.js
@@ -1,1 +1,7 @@
+// JavaScript, um das aktuelle Datum anzuzeigen
+document.addEventListener('DOMContentLoaded', function () {
+    const currentDateElement = document.getElementById('currentDate');
+    const currentDate = new Date();
+    currentDateElement.textContent = currentDate.toLocaleDateString();
+});
 
