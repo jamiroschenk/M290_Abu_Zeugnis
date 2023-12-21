3c8f09094a84af8d78a33ec99105492ee4904f88
jamiro.schenk <schenkj@bzz.ch>
2023-12-21 19:02:45 +0100

    code

--- abu_database.sql
+++ abu_database.sql
@@ -1,9 +1,27 @@
 -- Datenbank erstellen
 CREATE DATABASE IF NOT EXISTS schulzeugnis;
 
--- Wechsel in unsere Datenbank
 USE schulzeugnis;
 
+-- Tabelle für Schüler erstellen
+CREATE TABLE IF NOT EXISTS schueler (
+                                        schueler_id INT AUTO_INCREMENT PRIMARY KEY,
+                                        vorname VARCHAR(50) NOT NULL,
+                                        nachname VARCHAR(50) NOT NULL,
+                                        klasse VARCHAR(10) NOT NULL
+);
 
+-- Einträge für die Tabelle "schueler" einfügen
+INSERT INTO schueler (vorname, nachname, klasse) VALUES
+                                                     ('Max', 'Mustermann', 'ME23A'),
+                                                     ('Anna', 'Musterfrau', 'ME23B'),
+                                                     ('Tom', 'Schmidt', 'ME22A'),
+                                                     ('Julia', 'Schulz', 'ME22B'),
+                                                     ('David', 'Wagner', 'ME22B'),
+                                                     ('Laura', 'Müller', 'ME22A'),
+                                                     ('Daniel', 'Becker', 'ME23B'),
+                                                     ('Sophie', 'Koch', 'ME22A'),
+                                                     ('Michael', 'Bauer', 'ME23A'),
+                                                     ('Emma', 'Hoffmann', 'ME23B');
 
-
+SELECT * FROM schueler;
